f311e798097626a0f051adb3b9d495a894bbbeec
Flora Le <flora.le@monash.edu>
2023-01-20 21:57:44 +0700

    update code to set up simulation models

--- modelsetup.R
+++ modelsetup.R
@@ -23,53 +23,74 @@ library(ggsci)
 ##   0, 0, 1, -1, -1,
 ##   0, 0, 0, 1, -1), ncol=5, byrow=TRUE)
 
+# function for sim ilr
+
+# simulateData <- function(bm, wm, bcov, wcov, n, k, psi) {
+#   simd.b <- mvrnorm(n = n, mu = bm, Sigma = bcov)
+#   simd.b <- as.data.table(simd.b)
+# 
+#   inv.b <- ilrInv(simd.b[, .(V1, V2, V3, V4)], V = psi)
+#   inv.b <- clo(inv.b, total = 1440)
+# 
+#   simd.b <- as.data.table(inv.b)
+#   setnames(simd.b, c("BTST", "BWAKE", "BMVPA", "BLPA", "BSB"))
+# 
+#   simd.w <- mvrnorm(n = n * k, mu = wm, Sigma = wcov)
+#   simd.w <- as.data.table(simd.w)
+# 
+#   inv.w <- ilrInv(simd.w[, .(V1, V2, V3, V4)], V = psi)
+#   inv.w <- clo(inv.w, total = 5) # 5 because 1 for each means no change
+#   simd.w <- as.data.table(inv.w)
+#   setnames(simd.w, c("WTST", "WWAKE", "WMVPA", "WLPA", "WSB"))
+# 
+#   simd.b[, ID := seq_len(n)]
+#   simd.w[, ID := rep(seq_len(n), each = k)]
+# 
+#   simd.all <- merge(simd.b, simd.w, by = "ID")
+#   simd.all[, TST := WTST * BTST]
+#   simd.all[, WAKE := WWAKE * BWAKE]
+#   simd.all[, MVPA := WMVPA * BMVPA]
+#   simd.all[, LPA := WLPA * BLPA]
+#   simd.all[, SB := WSB * BSB]
+#   return(simd.all)
+# }
+
 simulateData <- function(bm, wm, bcov, wcov, n, k, psi) {
-  simd.b <- mvrnorm(n = n, mu = bm, Sigma = bcov)
+  simd.b <- rnorm.acomp(n = n, bm, bcov)
+  simd.b <- clo(simd.b, total = 1440)
   simd.b <- as.data.table(simd.b)
-
-  inv.b <- ilrInv(simd.b[, .(V1, V2, V3, V4)], V = psi)
-  inv.b <- clo(inv.b, total = 1440)
-
-  simd.b <- cbind(inv.b, simd.b[, .(BSTRESS, Age = exp(Age))])
-  simd.b[BSTRESS < 0, BSTRESS := 0]
-  simd.b[Age < 18, Age := 18]
-  setnames(simd.b, c("BTST", "BWAKE", "BMVPA", "BLPA", "BSB", "BSTRESS", "Age"))
-
-  simd.w <- mvrnorm(n = n * k, mu = wm, Sigma = wcov)
+  setnames(simd.b, c("BTST", "BWAKE", "BMVPA", "BLPA", "BSB"))
+  
+  simd.w <- rnorm.acomp(n = n * k, wm, wcov)
+  simd.w <- clo(simd.w, total = 5)
   simd.w <- as.data.table(simd.w)
-
-  inv.w <- ilrInv(simd.w[, .(V1, V2, V3, V4)], V = psi)
-  inv.w <- clo(inv.w, total = 5) # 5 because 1 for each means no change
-  simd.w <- cbind(inv.w, simd.w[, .(WSTRESS)])
-  setnames(simd.w, c("WTST", "WWAKE", "WMVPA", "WLPA", "WSB", "WSTRESS"))
-
+  setnames(simd.w, c("WTST", "WWAKE", "WMVPA", "WLPA", "WSB"))
+  
   simd.b[, ID := seq_len(n)]
   simd.w[, ID := rep(seq_len(n), each = k)]
-
+  
   simd.all <- merge(simd.b, simd.w, by = "ID")
   simd.all[, TST := WTST * BTST]
   simd.all[, WAKE := WWAKE * BWAKE]
   simd.all[, MVPA := WMVPA * BMVPA]
   simd.all[, LPA := WLPA * BLPA]
   simd.all[, SB := WSB * BSB]
-  simd.all[, STRESS := WSTRESS + BSTRESS]
   return(simd.all)
 }
 
 simmodel <- function(database, sbpbase, prefit = NULL) {
   
-  psub <- possub(c("TST", "WAKE", "MVPA", "LPA", "SB"))
+  psub <- basesub(c("TST", "WAKE", "MVPA", "LPA", "SB"))
   parts <- colnames(psub)
   
   cilr <- compilr(database, sbpbase, parts, total = 1440, idvar = "ID")
 
-  prior <- c(
-    set_prior("normal(50, 5)", class = "Intercept"),
-    set_prior("normal(0,  4)", class = "b"),
-    set_prior("normal(0, 10)", class = "sigma"),
-    set_prior("normal(0, 10)", class = "sd", coef = "Intercept", group = "ID"),
-    set_prior("normal(0,  4)", class = "sd", coef = "wilr2", group = "ID"),
-    set_prior("lkj(1)", class = "cor"))
+  # prior <- c(
+  #   set_prior("normal(50, 5)", class = "Intercept"),
+  #   set_prior("normal(0,  4)", class = "b"),
+  #   set_prior("normal(0, 10)", class = "sigma"),
+  #   set_prior("normal(0, 10)", class = "sd", coef = "Intercept", group = "ID"),
+  #   set_prior("lkj(1)", class = "cor"))
 
   ## priordat <- data.table(x = seq(from = -100, to = 100), by = .01)
   ## ggplot(priordat, aes(x = x)) + ## fixed effects intercept
@@ -85,21 +106,21 @@ simmodel <- function(database, sbpbase, prefit = NULL) {
 
   if (isTRUE(is.null(prefit))) {
     model <- brmcoda(cilr,
-                     depression ~ bilr1 + bilr2 + bilr3 + bilr4 + wilr1 + wilr2 + wilr3 + wilr4 +
-                       (1 + wilr2 | ID), cores = 4, chains = 4, iter = 3000, warmup = 500,
-                     prior = prior, backend = "cmdstanr")
+                     sleepy ~ bilr1 + bilr2 + bilr3 + bilr4 + wilr1 + wilr2 + wilr3 + wilr4 +
+                       (1 | ID), cores = 4, chains = 4, iter = 3000, warmup = 500, backend = "cmdstanr")
   } else {
     model <- brmcoda(cilr,
-                     depression ~ bilr1 + bilr2 + bilr3 + bilr4 + wilr1 + wilr2 + wilr3 + wilr4 +
-                       (1 + wilr2 | ID), cores = 4, chains = 4, iter = 3000, warmup = 500,
-                     prior = prior, backend = "cmdstanr", fit = prefit)
+                     sleepy ~ bilr1 + bilr2 + bilr3 + bilr4 + wilr1 + wilr2 + wilr3 + wilr4 +
+                       (1 | ID), cores = 4, chains = 4, iter = 3000, warmup = 500, backend = "cmdstanr", fit = prefit)
   }
 
   summodel <- summary(model$Model)
   ndt <- sum(subset(nuts_params(model$Model), Parameter == "divergent__")$Value)
 
-  bsubm <- bsub(model, substitute = psub, minute = 30)
-  wsubm <- wsub(model, substitute = psub, minute = 30)
+  bsubm <- substitution(model, delta = 1:30,
+                        level = "between", type = "conditional")
+  wsubm <- substitution(model, delta = 1:30,
+                        level = "within", type = "conditional")
 
   out <- list(
     CompILR = cilr,
@@ -108,7 +129,7 @@ simmodel <- function(database, sbpbase, prefit = NULL) {
     WithinResult = wsubm,
     N = N,
     K = K,
-    ndt = ndt
+    ndt = ndt #number of divergent chains
   )
 
   if (isTRUE(is.null(prefit))) {
@@ -137,7 +158,9 @@ simmodel <- function(database, sbpbase, prefit = NULL) {
 
 ## conditions (1000 runs each condition)
 d <- as.data.table(expand.grid(N = c(30, 50, 360, 1200), 
-                               K = c(3, 5, 7, 7, 14)))
-d <- d[rep(seq_len(.N), each = 1000)]
+                               K = c(3, 5, 7, 14),
+                               mrint = summary(m0)$random$UID[1, 1],
+                               sdrint = summary(m0)$random$UID[1, 2]))
+d <- d[rep(seq_len(.N), 1000)]
 
-# meanscovs <- readRDS("meanscovs.RDS")
+meanscovs <- readRDS("meanscovs.RDS")
