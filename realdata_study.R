c6416b0e89fbba5ef4c1af6641a5a4d6a47d61c5
Flora Le <flora.le@monash.edu>
2023-01-20 22:00:33 +0700

    real data study
    for example and guide simulation study

---
+++ realdata_study.R
@@ -0,0 +1,103 @@
+#### Calculate Means & Covariance Matrix for multilevel compositional data simulation ####
+library(data.table)
+library(compositions)
+library(multilevelTools)
+library(multilevelcoda)
+library(lme4)
+library(brms)
+library(JWileymisc)
+
+# sbp & psi
+sbp <- matrix(c(
+  1, 1, -1,-1, -1,
+  1, -1, 0, 0, 0,
+  0, 0, 1, -1, -1,
+  0, 0, 0, 1, -1), ncol = 5, byrow = TRUE)
+psi <- gsi.buildilrBase(t(sbp))
+
+
+# read data ----------------------------------------------------------------------------------------
+if (Sys.info()[["sysname"]] %in% "Windows") {
+  loc.base <- "g:"
+} else if (Sys.info()[["sysname"]] %in% "Darwin") {
+  loc.base <- "/Volumes/GoogleDrive"
+}
+
+d <- as.data.table(readRDS(file.path(loc.base, "Shared drives/EMA_Studies/ema_studies.RDS")))
+
+setnames(d, "TSTacti", "TST")
+setnames(d, "MVPAPERCacti", "MVPA")
+setnames(d, "LIGHTPERCacti", "LPA")
+setnames(d, "SEDPERCacti", "SB")
+
+d <- d[!is.na(TST) & !is.na(MVPA) & !is.na(SOLRAWacti)] ## exclude surveys without sleep/acti data
+
+d[, TST := TST * 60]
+d[, WAKE := SOLRAWacti + WASORAWacti]
+d[, timeawake := (24 * 60) - TST - WAKE]
+d[, SB   := (SB   / 100) * timeawake]
+d[, LPA  := (LPA  / 100) * timeawake]
+d[, MVPA := (MVPA / 100) * timeawake]
+d[, totalhours := TST + WAKE + SB + LPA + MVPA]
+
+d[, c("BMVPA", "WMVPA") := meanDeviations(MVPA), by = UID]
+d[, c("BLPA", "WLPA") := meanDeviations(LPA), by = UID]
+d[, c("BSB", "WSB") := meanDeviations(SB), by = UID]
+d[, c("BWAKE", "WWAKE") := meanDeviations(WAKE), by = UID]
+d[, c("BTST", "WTST") := meanDeviations(TST), by = UID]
+d[, c("BSTRESS", "WSTRESS") := meanDeviations(STRESS), by = UID]
+
+## make within variable ratio of total to between
+d[, WMVPA := MVPA / BMVPA]
+d[, WLPA := LPA / BLPA]
+d[, WSB := SB / BSB]
+d[, WWAKE := WAKE / BWAKE]
+d[, WTST := TST / BTST]
+
+# make composition
+tilr <- ilr(acomp(d[, .(TST, WAKE, MVPA, LPA, SB)]), V = psi)
+bilr <- ilr(acomp(d[, .(BTST, BWAKE, BMVPA, BLPA, BSB)]), V = psi)
+wilr <- ilr(acomp(d[, .(WTST, WWAKE, WMVPA, WLPA, WSB)]), V = psi)
+
+colnames(tilr) <- c("ilr1", "ilr2", "ilr3", "ilr4")
+colnames(bilr) <- c("bilr1", "bilr2", "bilr3", "bilr4")
+colnames(wilr) <- c("wilr1", "wilr2", "wilr3", "wilr4")
+
+dd <- cbind(
+  tilr,
+  bilr,
+  wilr,
+  d[, .(COPEExpZ, COPEPrcZ, COPEMenZ, COPEEACZ, COPE_APZ, COPE_ApprZ, COPE_AvoiZ, 
+        SLEEPY, UID)])
+
+# descriptives
+
+egltable(c("SLEEPY", "TST", "WAKE", "MVPA", "LPA", "SB"), data = dd)
+table(dd$SLEEPY)
+
+# models - COPEEACZ COPEMenZ SLEEPY
+# mt <- brm(COPE_ApprZ ~ ilr1 + ilr2 + ilr3 + ilr4 + (1 | UID), data = dd)
+# summary(mt)
+
+m0 <- brm(SLEEPY ~ bilr1 + bilr2 + bilr3 + bilr4 +
+           wilr1 + wilr2 + wilr3 + wilr4 + (1 | UID), data = dd,
+         cores = 8, seed = 123)
+summary(m0)
+
+# multilevelcoda
+cilr <- compilr(d, sbp = sbp, 
+                parts = c("TST", "WAKE", "MVPA", "LPA", "SB"), idvar = "UID")
+m <- brmcoda(cilr,
+             SLEEPY ~ bilr1 + bilr2 + bilr3 + bilr4 +
+               wilr1 + wilr2 + wilr3 + wilr4 + (1 | UID), 
+             cores = 8, seed = 123)
+summary(m$Model)
+
+library(doFuture)
+registerDoFuture()
+plan(multisession, workers = 5)
+
+subm <- substitution(m, delta = 1:60, 
+                     level = c("between", "within"), type = c("conditional", "marginal"))
+
+registerDoSEQ()
