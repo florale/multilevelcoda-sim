2e0ee45b73f340f88e9b4142a33c551d2c1f2b39
Flora Le <flora.le@monash.edu>
2023-03-29 12:06:18 +1100

    sim analyses for 5-class composition

--- simmodel.R
+++ simmodel.R
@@ -23,6 +23,7 @@ meanscovs <- readRDS("meanscovs.RDS")
 groundtruth <- readRDS("groundtruth.RDS")
 
 ## conditions (1000 runs each condition) --------
+# 4*4*5
 cond <- as.data.table(expand.grid(N = c(30, 50, 360, 1200),
                                   K = c(3, 5, 7, 14),
                                   rint_sd = c(1, sqrt(.5), sqrt(1.5)),
@@ -55,32 +56,38 @@ cond[, condition := ifelse(rint_sd == 1 & res_sd == sqrt(.5), "REbase_RESsmall",
 
 ## functions ---------
 simulateData <- function(bm, wm, bcov, wcov, n, k, psi) {
-  simd.b <- rnorm.acomp(n = n, bm, bcov)
+  simd.b <- mvrnorm(n = n, mu = bm, Sigma = bcov)
   simd.b <- as.data.table(simd.b)
+
+  inv.b <- ilrInv(simd.b[, .(V1, V2, V3, V4)], V = psi)
+  simd.b <- as.data.table(inv.b)
   setnames(simd.b, c("BTST", "BWAKE", "BMVPA", "BLPA", "BSB"))
-  
-  simd.w <- rnorm.acomp(n = n * k, wm, wcov)
-  simd.w <- clo(simd.w, total = 5)
+
+  simd.w <- mvrnorm(n = n * k, mu = wm, Sigma = wcov)
   simd.w <- as.data.table(simd.w)
+
+  inv.w <- ilrInv(simd.w[, .(V1, V2, V3, V4)], V = psi)
+  inv.w <- clo(inv.w, total = 5) # 5 because 1 for each means no change
+  simd.w <- as.data.table(inv.w)
   setnames(simd.w, c("WTST", "WWAKE", "WMVPA", "WLPA", "WSB"))
-  
+
   simd.b[, ID := seq_len(n)]
   simd.w[, ID := rep(seq_len(n), each = k)]
-  
+
   simd.all <- merge(simd.b, simd.w, by = "ID")
   simd.all[, TST := WTST * BTST]
   simd.all[, WAKE := WWAKE * BWAKE]
   simd.all[, MVPA := WMVPA * BMVPA]
   simd.all[, LPA := WLPA * BLPA]
   simd.all[, SB := WSB * BSB]
-  
+
   simd.t <- simd.all[, .(TST, WAKE, MVPA, LPA, SB)]
   simd.t <- clo(simd.t, total = 1440)
   simd.t <- as.data.table(simd.t)
   setnames(simd.t, c("TST", "WAKE", "MVPA", "LPA", "SB"))
-  
+
   simd.t[, ID := rep(seq_len(n), each = k)]
-  
+
   return(simd.t)
 }
 
@@ -97,7 +104,7 @@ simmodel <- function(database, sbpbase, prefit = NULL) {
       m <- brmcoda(cilr, sleepy ~ bilr1 + bilr2 + bilr3 + bilr4 + wilr1 + wilr2 + wilr3 + wilr4 + (1 | ID),
                    cores = 4, chains = 4, iter = 3000, warmup = 500, backend = "cmdstanr")
       
-      submodel <- substitution(m, delta = 1:30,
+      submodel <- substitution(m, delta = c(10, 20, 30, 60),
                                level = c("between", "within"), 
                                type = "conditional")
       
@@ -114,7 +121,8 @@ simmodel <- function(database, sbpbase, prefit = NULL) {
           list(CompIlr = cilr,
                Model = fit),
           class = "brmcoda")
-        submodel <- substitution(m, delta = 1:30,
+        
+        submodel <- substitution(m, delta = c(10, 20, 30, 60),
                                  level = c("between", "within"),
                                  type = "conditional")
         
