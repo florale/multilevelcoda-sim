efeec7c5fe154d1346887ff18749f9db03f981bf
Flora Le <flora.le@monash.edu>
2023-04-28 00:38:40 +1000

    update code to match with paper and sim study

--- realdata_study.R
+++ realdata_study.R
@@ -1,11 +1,19 @@
 library(data.table)
+library(extraoperators)
 library(compositions)
-library(multilevelTools)
 library(multilevelcoda)
-library(lme4)
 library(brms)
-library(JWileymisc)
+library(cmdstanr)
+library(insight)
+library(MASS)
 
+library(doFuture)
+library(foreach)
+library(parallel)
+library(doRNG)
+library(future)
+library(multilevelTools)
+library(JWileymisc)
 
 # data ----------------------------------------------------------------------------------------
 d <- as.data.table(readRDS("/Users/florale/Library/CloudStorage/GoogleDrive-flora.le@monash.edu/Shared drives/EMA_Studies/ema_studies.RDS"))
@@ -40,6 +48,26 @@ d[, WWAKE := WAKE / BWAKE]
 d[, WTST := TST / BTST]
 
 # make composition
+sbp <- matrix(c(
+  1, 1, -1,-1, -1,
+  1, -1, 0, 0, 0,
+  0, 0, 1, -1, -1,
+  0, 0, 0, 1, -1), ncol = 5, byrow = TRUE)
+psi <- gsi.buildilrBase(t(sbp))
+
+# check NA and 0
+d[which(is.na(d$TST)), "UID"]
+d[which(is.na(d$WAKE)), "UID"]
+d[which(is.na(d$SB)), "UID"]
+d[which(is.na(d$LPA)), "UID"]
+d[which(is.na(d$MVPA)), "UID"]
+d[which(is.na(d$SLEEPY)), "UID"]
+
+parts = c("TST", "WAKE", "MVPA", "LPA", "SB")
+any(apply(d[, parts, with = FALSE], 2, function(x) x == 0))
+
+d <- d[!(TST == 0 | WAKE == 0 | MVPA == 0 | LPA == 0 | SB == 0) & !is.na(SLEEPY)]
+
 tilr <- ilr(acomp(d[, .(TST, WAKE, MVPA, LPA, SB)]), V = psi)
 bilr <- ilr(acomp(d[, .(BTST, BWAKE, BMVPA, BLPA, BSB)]), V = psi)
 wilr <- ilr(acomp(d[, .(WTST, WWAKE, WMVPA, WLPA, WSB)]), V = psi)
@@ -58,35 +86,47 @@ sbp5 <- matrix(c(
 psi5 <- gsi.buildilrBase(t(sbp5))
 
 # multilevelcoda
-cilr5 <- compilr(d, sbp = sbp5, 
-                parts = c("TST", "WAKE", "MVPA", "LPA", "SB"), idvar = "UID")
+cilr5 <- compilr(d, sbp = sbp5,
+                 parts = c("TST", "WAKE", "MVPA", "LPA", "SB"), idvar = "UID")
 m5 <- brmcoda(cilr5,
              SLEEPY ~ bilr1 + bilr2 + bilr3 + bilr4 +
                wilr1 + wilr2 + wilr3 + wilr4 + (1 | UID), 
-             cores = 8, seed = 123)
+             cores = 4,
+             chains = 4,
+             iter = 3000,
+             warmup = 500,
+             seed = 123,
+             backend = "cmdstanr"
+)
 summary(m5$Model)
 
-# manual coding
-dd <- cbind(
-  tilr,
-  bilr,
-  wilr,
-  d[, .(COPEExpZ, COPEPrcZ, COPEMenZ, COPEEACZ, COPE_APZ, COPE_ApprZ, COPE_AvoiZ, 
-        SLEEPY, UID)])
+submodel5 <- substitution(
+  m5,
+  delta = c(30),
+  level = c("between", "within"),
+  type = "conditional")
 
-# descriptives
-
-# egltable(c("SLEEPY", "TST", "WAKE", "MVPA", "LPA", "SB"), data = dd)
-table(dd$SLEEPY) # 1-5
-
-# models - COPEEACZ COPEMenZ SLEEPY
-# mt <- brm(COPE_ApprZ ~ ilr1 + ilr2 + ilr3 + ilr4 + (1 | UID), data = dd)
-# summary(mt)
-
-m0 <- brm(SLEEPY ~ bilr1 + bilr2 + bilr3 + bilr4 +
-            wilr1 + wilr2 + wilr3 + wilr4 + (1 | UID), data = dd,
-          cores = 8, seed = 123)
-summary(m0)
+# # manual coding
+# dd <- cbind(
+#   tilr,
+#   bilr,
+#   wilr,
+#   d[, .(COPEExpZ, COPEPrcZ, COPEMenZ, COPEEACZ, COPE_APZ, COPE_ApprZ, COPE_AvoiZ, 
+#         SLEEPY, UID)])
+# 
+# # descriptives
+# egltable(c("SLEEPY", "TST", "WAKE", "MVPA", "LPA", "SB"), data = d)
+# d[, index := 1:.N, by = UID]
+# table(dd$SLEEPY) # 1-5
+# 
+# # models - COPEEACZ COPEMenZ SLEEPY
+# # mt <- brm(COPE_ApprZ ~ ilr1 + ilr2 + ilr3 + ilr4 + (1 | UID), data = dd)
+# # summary(mt)
+# 
+# m0 <- brm(SLEEPY ~ bilr1 + bilr2 + bilr3 + bilr4 +
+#             wilr1 + wilr2 + wilr3 + wilr4 + (1 | UID), data = dd,
+#           cores = 4, seed = 123)
+# summary(m0)
 
 ### 4 class ----------
 sbp4 <- matrix(c(
@@ -101,8 +141,20 @@ cilr4 <- compilr(d, sbp = sbp4,
 m4 <- brmcoda(cilr4,
              SLEEPY ~ bilr1 + bilr2 + bilr3 + 
                wilr1 + wilr2 + wilr3 + (1 | UID), 
-             cores = 8, seed = 123)
+             cores = 4,
+             chains = 4,
+             iter = 3000,
+             warmup = 500,
+             seed = 123,
+             backend = "cmdstanr"
+             )
 summary(m4$Model)
+
+submodel4 <- substitution(
+  m4,
+  delta = c(30),
+  level = c("between", "within"),
+  type = "conditional")
 
 ### 3 class ----------
 sbp3 <- matrix(c(
@@ -116,8 +168,20 @@ cilr3 <- compilr(d, sbp = sbp3,
                 parts = c("Sleep", "PA", "SB"), idvar = "UID")
 m3 <- brmcoda(cilr3,
              SLEEPY ~ bilr1 + bilr2 + wilr1 + wilr2 + (1 | UID), 
-             cores = 8, seed = 123)
+             cores = 4,
+             chains = 4,
+             iter = 3000,
+             warmup = 500,
+             seed = 123,
+             backend = "cmdstanr"
+)
 summary(m3$Model)
+
+submodel3 <- substitution(
+  m3,
+  delta = c(30),
+  level = c("between", "within"),
+  type = "conditional")
 
 # library(doFuture)
 # registerDoFuture()
@@ -127,51 +191,3 @@ summary(m3$Model)
 #                      level = c("between", "within"), type = c("conditional", "marginal"))
 # 
 # registerDoSEQ()
-
-# save results to guide sim study ------------
-
-class5_result <- data.table(
-  b0      = summary(m5)$fixed[1, 1],
-  b_bilr1 = summary(m5)$fixed[2, 1],
-  b_bilr2 = summary(m5)$fixed[3, 1],
-  b_bilr3 = summary(m5)$fixed[4, 1],
-  b_bilr4 = summary(m5)$fixed[5, 1],
-  b_wilr1 = summary(m5)$fixed[6, 1],
-  b_wilr2 = summary(m5)$fixed[7, 1],
-  b_wilr3 = summary(m5)$fixed[8, 1],
-  b_wilr4 = summary(m5)$fixed[9, 1],
-  
-  ll_b0    = summary(m5)$fixed[1, 3],
-  ll_bilr1 = summary(m5 )$fixed[2, 3],
-  ll_bilr2 = summary(m5)$fixed[3, 3],
-  ll_bilr3 = summary(m5)$fixed[4, 3],
-  ll_bilr4 = summary(m5)$fixed[5, 3],
-  ll_wilr1 = summary(m5)$fixed[6, 3],
-  ll_wilr2 = summary(m5)$fixed[7, 3],
-  ll_wilr3 = summary(m5)$fixed[8, 3],
-  ll_wilr4 = summary(m5)$fixed[9, 3],
-  
-  ul_b0    = summary(m5)$fixed[1, 4],
-  ul_bilr1 = summary(m5)$fixed[2, 4],
-  ul_bilr2 = summary(m5)$fixed[3, 4],
-  ul_bilr3 = summary(m5)$fixed[4, 4],
-  ul_bilr4 = summary(m5)$fixed[5, 4],
-  ul_wilr1 = summary(m5)$fixed[6, 4],
-  ul_wilr2 = summary(m5)$fixed[7, 4],
-  ul_wilr3 = summary(m5)$fixed[8, 4],
-  ul_wilr4 = summary(m5)$fixed[9, 4],
-  
-  u0    = summary(m5)$random$UID[1, 1],
-  ll_u0 = summary(m5)$random$UID[1, 3],
-  ul_u0 = summary(m5)$random$UID[1, 4],
-  
-  sigma = summary(m5)$spec_pars[[1]],
-  ll_sigma = summary(m5)$spec_pars[[3]],
-  ul_sigma = summary(m5)$spec_pars[[4]]
-  )
-
-# colMeans( as.data.frame(ranef(m0)$UID)[ , 1, drop = FALSE])
-# ranef <- as.data.frame(ranef(m0)$UID)
-# resid <- residuals(m0)
-# resid_m <- colMeans(resid[, 1, drop = FALSE], )
-# resid_sd <- sd(resid[, 1])
