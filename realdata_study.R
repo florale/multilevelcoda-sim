ecaca7026293ecc8f3d93e941aee927f304fe88c
Flora Le <flora.le@monash.edu>
2023-05-14 21:35:06 +1000

    update real data study

--- realdata_study.R
+++ realdata_study.R
@@ -16,36 +16,7 @@ library(multilevelTools)
 library(JWileymisc)
 
 # data ----------------------------------------------------------------------------------------
-d <- as.data.table(readRDS("/Users/florale/Library/CloudStorage/GoogleDrive-flora.le@monash.edu/Shared drives/EMA_Studies/ema_studies.RDS"))
-
-setnames(d, "TSTacti", "TST")
-setnames(d, "MVPAPERCacti", "MVPA")
-setnames(d, "LIGHTPERCacti", "LPA")
-setnames(d, "SEDPERCacti", "SB")
-
-d <- d[!is.na(TST) & !is.na(MVPA) & !is.na(SOLRAWacti)] ## exclude surveys without sleep/acti data
-
-d[, TST := TST * 60]
-d[, WAKE := SOLRAWacti + WASORAWacti]
-d[, timeawake := (24 * 60) - TST - WAKE]
-d[, SB   := (SB   / 100) * timeawake]
-d[, LPA  := (LPA  / 100) * timeawake]
-d[, MVPA := (MVPA / 100) * timeawake]
-d[, totalhours := TST + WAKE + SB + LPA + MVPA]
-
-d[, c("BMVPA", "WMVPA") := meanDeviations(MVPA), by = UID]
-d[, c("BLPA", "WLPA") := meanDeviations(LPA), by = UID]
-d[, c("BSB", "WSB") := meanDeviations(SB), by = UID]
-d[, c("BWAKE", "WWAKE") := meanDeviations(WAKE), by = UID]
-d[, c("BTST", "WTST") := meanDeviations(TST), by = UID]
-d[, c("BSTRESS", "WSTRESS") := meanDeviations(STRESS), by = UID]
-
-## make within variable ratio of total to between
-d[, WMVPA := MVPA / BMVPA]
-d[, WLPA := LPA / BLPA]
-d[, WSB := SB / BSB]
-d[, WWAKE := WAKE / BWAKE]
-d[, WTST := TST / BTST]
+d <- as.data.table(readRDS("/Volumes/shared/Behavioral-med-lab/StressHealthStudy/SHS Research Interns/Data/shs_daily_ggir.RDS"))
 
 # make composition
 sbp <- matrix(c(
@@ -56,41 +27,35 @@ sbp <- matrix(c(
 psi <- gsi.buildilrBase(t(sbp))
 
 # check NA and 0
-d[which(is.na(d$TST)), "UID"]
-d[which(is.na(d$WAKE)), "UID"]
-d[which(is.na(d$SB)), "UID"]
-d[which(is.na(d$LPA)), "UID"]
-d[which(is.na(d$MVPA)), "UID"]
-d[which(is.na(d$SLEEPY)), "UID"]
+# d[which(is.na(d$Sleepg)), "ID"]
+# d[which(is.na(d$WAKEg)), "ID"]
+# d[which(is.na(d$SBg)), "ID"]
+# d[which(is.na(d$LPAg)), "ID"]
+# d[which(is.na(d$MVPAg)), "ID"]
+# d[which(is.na(d$SLEEPY)), "ID"]
 
-parts = c("TST", "WAKE", "MVPA", "LPA", "SB")
+parts5 <- c("Sleepg", "WAKEg", "MVPAg", "LPAg", "SBg")
 any(apply(d[, parts, with = FALSE], 2, function(x) x == 0))
 
-d <- d[!(TST == 0 | WAKE == 0 | MVPA == 0 | LPA == 0 | SB == 0) & !is.na(SLEEPY)]
+# d <- d[!(TST == 0 | WAKE == 0 | MVPA == 0 | LPA == 0 | SB == 0) & !is.na(SLEEPY)]
 
-tilr <- ilr(acomp(d[, .(TST, WAKE, MVPA, LPA, SB)]), V = psi)
-bilr <- ilr(acomp(d[, .(BTST, BWAKE, BMVPA, BLPA, BSB)]), V = psi)
-wilr <- ilr(acomp(d[, .(WTST, WWAKE, WMVPA, WLPA, WSB)]), V = psi)
+d <- d[!is.na(Sleepg) & !is.na(WAKEg) & !is.na(MVPAg) & !is.na(LPAg) & !is.na(SBg)]
 
-colnames(tilr) <- c("ilr1", "ilr2", "ilr3", "ilr4")
-colnames(bilr) <- c("bilr1", "bilr2", "bilr3", "bilr4")
-colnames(wilr) <- c("wilr1", "wilr2", "wilr3", "wilr4")
+# tilr <- ilr(acomp(d[, .(TST, WAKE, MVPA, LPA, SB)]), V = psi)
+# bilr <- ilr(acomp(d[, .(BTST, BWAKE, BMVPA, BLPA, BSB)]), V = psi)
+# wilr <- ilr(acomp(d[, .(WTST, WWAKE, WMVPA, WLPA, WSB)]), V = psi)
+# 
+# colnames(tilr) <- c("ilr1", "ilr2", "ilr3", "ilr4")
+# colnames(bilr) <- c("bilr1", "bilr2", "bilr3", "bilr4")
+# colnames(wilr) <- c("wilr1", "wilr2", "wilr3", "wilr4")
 
 ### 5 class ----------
-# sbp & psi
-sbp5 <- matrix(c(
-  1, 1, -1,-1, -1,
-  1, -1, 0, 0, 0,
-  0, 0, 1, -1, -1,
-  0, 0, 0, 1, -1), ncol = 5, byrow = TRUE)
-psi5 <- gsi.buildilrBase(t(sbp5))
-
 # multilevelcoda
 cilr5 <- compilr(d, sbp = sbp5,
-                 parts = c("TST", "WAKE", "MVPA", "LPA", "SB"), idvar = "UID")
+                 parts = parts5, idvar = "ID")
 m5 <- brmcoda(cilr5,
-             SLEEPY ~ bilr1 + bilr2 + bilr3 + bilr4 +
-               wilr1 + wilr2 + wilr3 + wilr4 + (1 | UID), 
+              SLEEPYNextDay ~ bilr1 + bilr2 + bilr3 + bilr4 +
+               wilr1 + wilr2 + wilr3 + wilr4 + (1 | ID), 
              cores = 4,
              chains = 4,
              iter = 3000,
@@ -100,31 +65,57 @@ m5 <- brmcoda(cilr5,
 )
 summary(m5$Model)
 
+registerDoFuture()
+plan(multisession, workers = 5)
 submodel5 <- substitution(
   m5,
-  delta = c(30),
+  delta = c(1:10),
   level = c("between", "within"),
   type = "conditional")
+registerDoSEQ()
 
+plotsub(submodel5$BetweenpersonSub$Sleepg, x = "Sleep", y = "Sleepy")
+plotsub(submodel5$BetweenpersonSub$WAKEg, x = "AWAKE", y = "Sleepy")
+plotsub(submodel5$BetweenpersonSub$MVPAg, x = "MVPA", y = "Sleepy")
+plotsub(submodel5$BetweenpersonSub$LPAg, x = "LPA", y = "Sleepy")
+plotsub(submodel5$BetweenpersonSub$SBg, x = "SB", y = "Sleepy")
+
+plotsub(submodel5$WithinpersonSub$Sleepg, x = "Sleep", y = "Sleepy")
+plotsub(submodel5$WithinpersonSub$WAKEg, x = "AWAKE", y = "Sleepy")
+plotsub(submodel5$WithinpersonSub$MVPAg, x = "MVPA", y = "Sleepy")
+plotsub(submodel5$WithinpersonSub$LPAg, x = "LPA", y = "Sleepy")
+plotsub(submodel5$WithinpersonSub$SBg, x = "SB", y = "Sleepy")
+
+plotsub(submodel5$BetweenpersonSubMargin$Sleepg, x = "Sleep", y = "Sleepy")
+plotsub(submodel5$BetweenpersonSubMargin$WAKEg, x = "AWAKE", y = "Sleepy")
+plotsub(submodel5$BetweenpersonSubMargin$MVPAg, x = "MVPA", y = "Sleepy")
+plotsub(submodel5$BetweenpersonSubMargin$LPAg, x = "LPA", y = "Sleepy")
+plotsub(submodel5$BetweenpersonSubMargin$SBg, x = "SB", y = "Sleepy")
+
+plotsub(submodel5$WithinpersonSubMargin$Sleepg, x = "Sleep", y = "Sleepy")
+plotsub(submodel5$WithinpersonSubMargin$WAKEg, x = "AWAKE", y = "Sleepy")
+plotsub(submodel5$WithinpersonSubMargin$MVPAg, x = "MVPA", y = "Sleepy")
+plotsub(submodel5$WithinpersonSubMargin$LPAg, x = "LPA", y = "Sleepy")
+plotsub(submodel5$WithinpersonSubMargin$SBg, x = "SB", y = "Sleepy")
 # # manual coding
 # dd <- cbind(
 #   tilr,
 #   bilr,
 #   wilr,
 #   d[, .(COPEExpZ, COPEPrcZ, COPEMenZ, COPEEACZ, COPE_APZ, COPE_ApprZ, COPE_AvoiZ, 
-#         SLEEPY, UID)])
+#         SLEEPY, ID)])
 # 
 # # descriptives
 # egltable(c("SLEEPY", "TST", "WAKE", "MVPA", "LPA", "SB"), data = d)
-# d[, index := 1:.N, by = UID]
+# d[, index := 1:.N, by = ID]
 # table(dd$SLEEPY) # 1-5
 # 
 # # models - COPEEACZ COPEMenZ SLEEPY
-# # mt <- brm(COPE_ApprZ ~ ilr1 + ilr2 + ilr3 + ilr4 + (1 | UID), data = dd)
+# # mt <- brm(COPE_ApprZ ~ ilr1 + ilr2 + ilr3 + ilr4 + (1 | ID), data = dd)
 # # summary(mt)
 # 
 # m0 <- brm(SLEEPY ~ bilr1 + bilr2 + bilr3 + bilr4 +
-#             wilr1 + wilr2 + wilr3 + wilr4 + (1 | UID), data = dd,
+#             wilr1 + wilr2 + wilr3 + wilr4 + (1 | ID), data = dd,
 #           cores = 4, seed = 123)
 # summary(m0)
 
@@ -133,14 +124,17 @@ sbp4 <- matrix(c(
   1, -1, -1,-1,
   0, 1, -1, -1,
   0, 0, 1, -1), ncol = 4, byrow = TRUE)
-psi4 <- gsi.buildilrBase(t(sbp4))
+# psi4 <- gsi.buildilrBase(t(sbp4))
 
-d[, Sleep := TST + WAKE]
+d[, TIBRAWg := TIBg]
+
+d[, TIBg := Sleepg + WAKEg]
 cilr4 <- compilr(d, sbp = sbp4, 
-                parts = c("Sleep", "MVPA", "LPA", "SB"), idvar = "UID")
+                parts = c("TIBg", "MVPAg", "LPAg", "SBg"), idvar = "ID")
+
 m4 <- brmcoda(cilr4,
-             SLEEPY ~ bilr1 + bilr2 + bilr3 + 
-               wilr1 + wilr2 + wilr3 + (1 | UID), 
+              SLEEPYNextDay ~ bilr1 + bilr2 + bilr3 + 
+               wilr1 + wilr2 + wilr3 + (1 | ID), 
              cores = 4,
              chains = 4,
              iter = 3000,
@@ -163,11 +157,12 @@ sbp3 <- matrix(c(
   ncol = 3, byrow = TRUE)
 psi3 <- gsi.buildilrBase(t(sbp3))
 
-d[, PA := MVPA + LPA]
+d[, PAg := MVPAg + LPAg]
 cilr3 <- compilr(d, sbp = sbp3, 
-                parts = c("Sleep", "PA", "SB"), idvar = "UID")
+                parts = c("TIBg", "PAg", "SBg"), idvar = "ID")
+
 m3 <- brmcoda(cilr3,
-             SLEEPY ~ bilr1 + bilr2 + wilr1 + wilr2 + (1 | UID), 
+              SLEEPYNextDay ~ bilr1 + bilr2 + wilr1 + wilr2 + (1 | ID), 
              cores = 4,
              chains = 4,
              iter = 3000,
