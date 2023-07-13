43da62ecffd9ea6a279d76644f6bd9ef5697a924
Flora Le <flora.le@monash.edu>
2023-07-13 21:40:12 +1000

    move shiny app to separate repo

--- multilevelcoda-sim-shiny/app.R
+++
@@ -1,7 +0,0 @@
-source("functions.R")
-source("multilevelcoda-sim-shiny/shiny_in.R")
-
-source("multilevelcoda-sim-shiny/server.R")
-source("multilevelcoda-sim-shiny/ui.R")
-
-shinyApp(ui = ui, server = server)
